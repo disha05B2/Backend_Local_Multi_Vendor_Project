fafe8ef063af0b8a2f4919057274fd26452cce90
disha05B2 <dishareddy0307@gmail.com>
2024-06-02 22:28:27 +0530

    error rectified

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const vendorRoutes=require('./routes/vendorRoutes')
 const bodyParser=require('body-parser');
 const firmRoutes = require('./routes/firmRoutes');
 const productRoutes = require('./routes/productRoutes');
-// const cors=require('cors');
+const cors=require('cors');
 const path = require('path');
 const app=express()
 const PORT = process.env.PORT || 4000;
 dotEnv.config();
+app.use(cors())
 mongoose.connect(process.env.MONGO_URI)
     .then(()=>console.log("MongoDB connected successfully"))
     .catch((error)=>console.log(error))
