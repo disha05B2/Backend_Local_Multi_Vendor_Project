bbb4ce6f6f0e5e938251dcd5edade482608a58d6
disha05B2 <dishareddy0307@gmail.com>
2024-06-02 22:21:28 +0530

    error rectified

--- routes/firmRoutes.js
+++ routes/firmRoutes.js
@@ -8,6 +8,6 @@ router.get('/uploads/:imageName',(req,res)=>{
     res.headersSent('Content-Type','image/jpeg');
     res.sendFile(path.join(__dirname,'..','uploads',imageName));
 });
-router.delete('/:firmId',FirmController.deleteFirmById);
+router.delete('/:firmId',firmController.deleteFirmById);
 module.exports = router;
 
